e2846b2a8e3a6c6666d4e65768ad0e7ff7f4f736
KianDoyleSparta <kdoyle@spartaglobal.com>
2024-09-27 20:45:19 +0100

    First Commit: Setting up project

---
+++ src/main/java/com/sparta/kd/chatbotapp/ChatBotAppApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.kd.chatbotapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatBotAppApplication.class, args);
+    }
+
+}
