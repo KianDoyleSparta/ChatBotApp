88c5821e9d81ef3647f6510c79d7623fe4d01e98
KianDoyleSparta <kdoyle@spartaglobal.com>
2024-09-29 23:04:46 +0100

    Update: Updated UI
    - Updated UI to more closely match figma file
    - Updated conversation return to return conversations in order

--- src/main/java/com/sparta/kd/chatbotapp/Utilities/ResponseFormatter.java
+++ src/main/java/com/sparta/kd/chatbotapp/Utilities/ResponseFormatter.java
@@ -10,13 +10,13 @@ public class ResponseFormatter {
 
     public String formatFaqResponse(ArrayList<Faq> response) {
         if (response.isEmpty()) {
-            return "I'm sorry, I couldn't quote find the answer in our database. Could you please be more specific?";
+            return "I'm sorry, I couldn't quite find the answer in our database. Could you please be more specific?";
         } else {
             StringBuilder sb = new StringBuilder();
             for (Faq faq : response) {
                 sb.append(faq.getAnswer()).append("\n");
             }
-            return "I found some related common FAQs.\n" + sb.toString() + "\nDid these help answer your question? (Yes/No)";
+            return "I found some related common FAQs. " + sb.toString() + " Did these help answer your question? (Yes/No)";
         }
     }
 }
